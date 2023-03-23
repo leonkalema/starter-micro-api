558044f2d03f464a343c389fe52542849baa497a
leonkalema <tullapeople@gmail.com>
2023-03-23 09:34:59 +0100

    have pdf data from blob

--- generate-pdf.js
+++ generate-pdf.js
@@ -1,19 +1,29 @@
 const jsPDF = require('jspdf').default;
-
 const autoTable = require('jspdf-autotable');
 
-
-
-async function generatePDF(htmlContent, watermark) {
-   const pdf = new jsPDF();
- pdf.setFontSize(10);
+async function generatePDF(workoutPlan, watermark) {
+  const pdf = new jsPDF();
+  pdf.setFontSize(10);
   pdf.text(watermark, pdf.internal.pageSize.getWidth() - 20, pdf.internal.pageSize.getHeight() - 10);
+
+  const tableHeaders = ['Exercise', 'Reps', 'Sets', 'Rest'];
+  const tableRows = [];
+
+  // Format the workout plan data into rows
+  workoutPlan.forEach((day) => {
+    tableRows.push([day.day, '', '', '']);
+
+    day.exercises.forEach((exercise) => {
+      tableRows.push([exercise.name, exercise.reps, exercise.sets, exercise.rest]);
+    });
+  });
+
   pdf.autoTable({
-    html: htmlContent,
+    head: [tableHeaders],
+    body: tableRows,
   });
 
   return pdf.output('blob');
 }
 
 module.exports = generatePDF;
-
