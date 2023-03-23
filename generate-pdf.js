714f0c8dcd5f4afbbea0d89945f36a53e9b03064
leonkalema <tullapeople@gmail.com>
2023-03-23 04:21:58 +0100

    pdf efusse pdf

--- generate-pdf.js
+++ generate-pdf.js
@@ -1,10 +1,11 @@
 const jsPDF = require('jspdf');
 const autoTable = require('jspdf-autotable');
-const pdf = new jsPDF();
+
 
 
 async function generatePDF(htmlContent, watermark) {
-pdf.setFontSize(10);
+   const pdf = new jsPDF();
+ pdf.setFontSize(10);
   pdf.text(watermark, pdf.internal.pageSize.getWidth() - 20, pdf.internal.pageSize.getHeight() - 10);
   pdf.autoTable({
     html: htmlContent,
